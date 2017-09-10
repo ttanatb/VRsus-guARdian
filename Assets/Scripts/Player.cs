ec76ecc86cca186f5bf26bd9969980370100a052
ttanatb <ttanatb@gmail.com>
2017-09-10 19:38:45 -0400

    Fixed networking player logic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public enum PlayerType
+{
+    AR,
+    VR
+}
+
+public class Player : NetworkBehaviour
+{
+    public GameObject ARPlayer;
+    public GameObject VRPlayer;
+
+    private AudioListener[] listeners;
+    private Camera[] cameras;
+
+    private PlayerType playerType;
+
+    private void Awake()
+    {
+        listeners = GetComponentsInChildren<AudioListener>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!ARPlayer)
+        {
+            ARPlayer = transform.GetChild(0).gameObject;
+        }
+
+        if (!VRPlayer)
+        {
+            VRPlayer = transform.GetChild(1).gameObject;
+        }
+
+
+
+#if UNITY_IOS
+         playerType = PlayerType.AR;
+#else
+        playerType = PlayerType.VR;
+#endif
+
+        if (!isClient)
+        {
+            foreach(AudioListener listener in listeners)
+            {
+                listener.enabled = false;
+            }
+
+            foreach(Camera cam in cameras)
+            {
+                cam.enabled = false;
+            }
+        }
+
+        if (playerType == PlayerType.AR)
+        {
+            foreach(Camera cam in VRPlayer.GetComponentsInChildren<Camera>())
+            {
+                cam.enabled = false;
+                cam.GetComponent<AudioListener>().enabled = false;
+            }
+
+            foreach(GameObject obj in GameObject.FindGameObjectsWithTag("VROnly"))
+            {
+                obj.SetActive(false);
+            }
+        }
+
+        else
+        {
+            foreach (Camera cam in ARPlayer.GetComponentsInChildren<Camera>())
+            {
+                cam.enabled = false;
+                cam.GetComponent<AudioListener>().enabled = false;
+            }
+
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("AROnly"))
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
