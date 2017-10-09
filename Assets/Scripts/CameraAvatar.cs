79a5693f6c998e15d1496e54ed4eec9737e92c04
ttanatb <ttanatb@gmail.com>
2017-10-09 19:31:31 -0400

    Fixed issue with missing avatar

--- Assets/Scripts/CameraAvatar.cs
+++ Assets/Scripts/CameraAvatar.cs
@@ -5,12 +5,4 @@ using UnityEngine;
 public class CameraAvatar : MonoBehaviour {
 
     public Combat rootPlayer;
-
-#if UNITY_IOS
-    private void Start()
-    {
-        GetComponent<Renderer>().enabled = false;
-    }
-#endif
-
 }
