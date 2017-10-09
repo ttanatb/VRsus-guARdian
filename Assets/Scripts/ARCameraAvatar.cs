7b5757280a3a5c34b3387c90df6ab5316b8b215b
ttanatb <ttanatb@gmail.com>
2017-10-09 18:37:30 -0400

    Fixed AR player collision issues

--- Assets/Scripts/ARCameraAvatar.cs
+++ Assets/Scripts/ARCameraAvatar.cs
@@ -6,4 +6,11 @@ public class ARCameraAvatar : MonoBehaviour {
 
     public GameObject rootPlayer;
 
+#if UNITY_IOS
+    private void Start()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+#endif
+
 }
