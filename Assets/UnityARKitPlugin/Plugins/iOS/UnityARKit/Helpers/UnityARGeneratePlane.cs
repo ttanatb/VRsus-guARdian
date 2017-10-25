bf5c40815f141aab1dce9145b821a13b0305f25e
Tanat Boozayaangool <ttanatb@gmail.com>
2017-10-24 20:53:24 -0400

    Fixed issue with plane generator

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARGeneratePlane.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARGeneratePlane.cs
@@ -21,16 +21,16 @@ namespace UnityEngine.XR.iOS
             unityARAnchorManager.Destroy();
         }
 
-        void OnGUI()
-        {
-            List<ARPlaneAnchorGameObject> arpags = unityARAnchorManager.GetCurrentPlaneAnchors();
-            if (arpags.Count >= 1)
-            {
-                //ARPlaneAnchor ap = arpags [0].planeAnchor;
-                //GUI.Box (new Rect (100, 100, 800, 60), string.Format ("Center: x:{0}, y:{1}, z:{2}", ap.center.x, ap.center.y, ap.center.z));
-                //GUI.Box(new Rect(100, 200, 800, 60), string.Format ("Extent: x:{0}, y:{1}, z:{2}", ap.extent.x, ap.extent.y, ap.extent.z));
-            }
-        }
+		int prevCount = 0;
+		void Update() {
+			List<ARPlaneAnchorGameObject> arpags = unityARAnchorManager.GetCurrentPlaneAnchors();
+
+			if (arpags.Count != prevCount) {
+				Debug.Log ("Added plane");
+			}
+
+			prevCount = arpags.Count;
+		}
 #endif
     }
 }
