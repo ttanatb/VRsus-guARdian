8e48fe01031ae919fe0314146b011db4b8479210
SPCoffey <sean@coffeyhouse.com>
2018-02-03 18:59:57 -0500

    Lowpoly shading added to terrain

--- Assets/Scripts/GameObjects/Environment/EnvironmentCreation.cs
+++ Assets/Scripts/GameObjects/Environment/EnvironmentCreation.cs
@@ -22,7 +22,10 @@ public class EnvironmentCreation : MonoBehaviour {
     private List<Vector2> mountainUVs;
     private Vector3 center;
     private int angles;
+    private int tripleAngles;
     private int layers;
+
+    private Mesh plains;
 
     public void CreateTerrain ()
     {
@@ -30,6 +33,7 @@ public class EnvironmentCreation : MonoBehaviour {
         {
             layers = 1;
             angles = boundary.Count;
+            tripleAngles = angles * 3;
             mountainVerts = new List<Vector3>(angles * 2);
             mountainUVs = new List<Vector2>(angles * 2);
             mountainTris = new List<int>(6 * angles);
@@ -38,7 +42,12 @@ public class EnvironmentCreation : MonoBehaviour {
             foreach (Vector3 vert in boundary)
             {
                 center += vert;
+                // Triple for hard edges
                 mountainVerts.Add(vert);
+                mountainVerts.Add(vert);
+                mountainVerts.Add(vert);
+                mountainUVs.Add(new Vector2(vert.x, vert.z));
+                mountainUVs.Add(new Vector2(vert.x, vert.z));
                 mountainUVs.Add(new Vector2(vert.x, vert.z));
             }
         
@@ -74,36 +83,41 @@ public class EnvironmentCreation : MonoBehaviour {
         layers++;
 
         int curMountVerts = mountainVerts.Count;
-        int extrudeStart = curMountVerts - angles;
+        int extrudeStart = curMountVerts - tripleAngles;
 
-        for (int i = curMountVerts - angles; i < curMountVerts; i++)
+        for (int i = curMountVerts - tripleAngles; i < curMountVerts; i += 3)
         {
             Vector3 curMount = mountainVerts[i];
             Vector3 centerToMount = curMount - center;
             Vector3 temp = curMount + (centerToMount.normalized * layerWidth);
 
+            // Triple for hard edges
+            mountainVerts.Add(temp);
+            mountainVerts.Add(temp);
             mountainVerts.Add(temp);
             mountainUVs.Add(new Vector2(temp.x, temp.z));
+            mountainUVs.Add(new Vector2(temp.x, temp.z));
+            mountainUVs.Add(new Vector2(temp.x, temp.z));
 
-            if (i == curMountVerts - 1)
+            if (i >= curMountVerts - 3)
             {
                 mountainTris.Add(i);
-                mountainTris.Add(i + angles);
-                mountainTris.Add(extrudeStart);
+                mountainTris.Add(i + tripleAngles);
+                mountainTris.Add(extrudeStart + 2);
 
-                mountainTris.Add(extrudeStart);
-                mountainTris.Add(i + angles);
-                mountainTris.Add(extrudeStart + angles);
+                mountainTris.Add(extrudeStart + 1);
+                mountainTris.Add(i + 1 + tripleAngles);
+                mountainTris.Add(extrudeStart + 2 + tripleAngles);
             }
             else
             {
                 mountainTris.Add(i);
-                mountainTris.Add(i + angles);
-                mountainTris.Add(i + 1);
+                mountainTris.Add(i + tripleAngles);
+                mountainTris.Add(i + 5);
 
-                mountainTris.Add(i + 1);
-                mountainTris.Add(i + angles);
-                mountainTris.Add(i + 1 + angles);
+                mountainTris.Add(i + 4);
+                mountainTris.Add(i + 1 + tripleAngles);
+                mountainTris.Add(i + 5 + tripleAngles);
             }
         }
 
@@ -115,14 +129,14 @@ public class EnvironmentCreation : MonoBehaviour {
         int peakLayer = layers - peakOffset;
         float yRange = yMax - yMin;
         float yIncrement = yRange / peakLayer;
-        int startIndex = angles;
+        int startIndex = tripleAngles;
         int endIndex = mountainVerts.Count;
 
-        for (int i = startIndex; i < endIndex; i++)
+        for (int i = startIndex; i < endIndex; i += 3)
         {
             Vector3 temp = mountainVerts[i];
 
-            int curLayer = i / angles;
+            int curLayer = i / tripleAngles;
 
             float standardHeight = 0;
 
@@ -137,32 +151,38 @@ public class EnvironmentCreation : MonoBehaviour {
 
             temp.y = standardHeight + Random.Range(-yVariance, yVariance);
             mountainVerts[i] = temp;
+            mountainVerts[i + 1] = temp;
+            mountainVerts[i + 2] = temp;
         }
     }
 
     private void CreateCliffs()
     {
         int cliffSides = Random.Range(minCliffSides, maxCliffSides + 1);
-        int cliffStart = Random.Range(0, angles);
-        int cliffEnd = cliffStart + cliffSides;
+        int cliffStart = Random.Range(0, angles) * 3;
+        int cliffEnd = cliffStart + (cliffSides * 3);
 
-        for (int i = cliffStart; i < cliffEnd; i++)
+        for (int i = cliffStart; i < cliffEnd; i += 3)
         {
             for (int k = 1; k < layers; k++)
             {
                 Vector3 temp = Vector3.zero;
 
-                if (i >= angles)
+                if (i >= tripleAngles)
                 {
-                    temp = mountainVerts[(i - angles) + (angles * k)];
+                    temp = mountainVerts[(i - tripleAngles) + (tripleAngles * k)];
                     temp.y = cliffY;
-                    mountainVerts[(i - angles) + (angles * k)] = temp;
+                    mountainVerts[(i - tripleAngles) + (tripleAngles * k)] = temp;
+                    mountainVerts[(i - tripleAngles) + 1 + (tripleAngles * k)] = temp;
+                    mountainVerts[(i - tripleAngles) + 2 + (tripleAngles * k)] = temp;
                 }
                 else
                 {
-                    temp = mountainVerts[i + (angles * k)];
+                    temp = mountainVerts[i + (tripleAngles * k)];
                     temp.y = cliffY;
-                    mountainVerts[i + (angles * k)] = temp;
+                    mountainVerts[i + (tripleAngles * k)] = temp;
+                    mountainVerts[i + 1 + (tripleAngles * k)] = temp;
+                    mountainVerts[i + 2 + (tripleAngles * k)] = temp;
                 }
             }
         }
