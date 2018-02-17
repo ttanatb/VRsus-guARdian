ff8f827c2332aa77bafff18af4fe45d3d2f58e3b
Sean Coffey <spc7194@g.rit.edu>
2018-02-17 17:59:12 -0500

    Tested arm Added launcher

---
+++ Assets/Scripts/VR/ObjectLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLauncher : MonoBehaviour {
+
+    public GameObject launchObject;
+    public float force;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("RightTrigger"))
+        {
+            GameObject temp = Instantiate(launchObject, transform.position, transform.rotation);
+            temp.GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
+        }
+	}
+}
